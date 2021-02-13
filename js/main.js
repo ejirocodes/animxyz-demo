4f9237edc85bf2d6dbcc9890f4dffa9856de0f0f
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2021-02-13 13:33:03 +0100

    update demo

--- js/main.js
+++ js/main.js
@@ -1,17 +1,36 @@
-const glass = document.querySelector('#glass')
+// let shapes = document.querySelectorAll('.shape')
+
+// function animateGlass(selector) {
+//     for (let i = 0; i < selector.length; i++) {
+//         console.log(selector[i].classList.contains('xyz-in'));
+//         if (selector[i].classList.contains('xyz-in')) {
+//             selector[i].classList.remove('xyz-in')
+//             selector[i].classList.add('xyz-out')
+//         } else {
+//             selector[i].classList.remove('xyz-out')
+//             selector[i].classList.add('xyz-in')
+//         }
+//     }
+
+// }
+
+
+// setInterval(() => {
+//     animateGlass(shapes)
+// }, 2000);
+
+const glass = document.querySelector("#glass");
 
 function animateGlass(selector) {
-    if (selector.classList.contains('xyz-in')) {
-        selector.classList.remove('xyz-in')
-        selector.classList.add('xyz-out')
-    } else {
-        selector.classList.remove('xyz-out')
-        selector.classList.add('xyz-in')
-    }
-    
+  if (selector.classList.contains("xyz-in")) {
+    selector.classList.remove("xyz-in");
+    selector.classList.add("xyz-out");
+  } else {
+    selector.classList.remove("xyz-out");
+    selector.classList.add("xyz-in");
+  }
 }
 
-
 setInterval(() => {
-    animateGlass(glass)
-}, 2000);
+  animateGlass(glass);
+}, 2000)
